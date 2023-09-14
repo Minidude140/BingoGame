0b47eff1dcf58012a3b2d88d85df2f1510cdc3ee
Zac <zac.minidude140@Gmail.com>
2023-09-14 08:25:15 -0600

    added TODO list

--- BingoGame/BingoGame.vb
+++ BingoGame/BingoGame.vb
@@ -1,7 +1,7 @@
 ﻿'Zachary Christensen
 'RCET 2265
 'Fall 2023
-'Bingo Game
+'Bingo Game (in class example)
 'https://github.com/Minidude140/BingoGame.git
 
 
@@ -9,6 +9,15 @@ Option Explicit On
 Option Strict On
 
 Module BingoGame
+
+    'TODO
+    '[]Create an array to track the numbers called
+    '[]create a function to display the balls already called
+    '[]create a way to draw a ball, check if it has been called, and track it in...
+    '[]create a way to restart the game.  Should happen automatically if all balls are called
+    '[]new game, play again, or quit functionality
+    '[]generate player cards and make playable
+    '[]create a way to display the bingo card
 
     Sub Main()
 
