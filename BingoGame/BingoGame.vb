6f77731ce0c01e23b681802887fa18705b2d20ef
Zac <zac.minidude140@Gmail.com>
2023-09-18 08:55:16 -0600

    Added randomNumber() function and DrawBall() sub. added main loop and quit functionality

--- BingoGame/BingoGame.vb
+++ BingoGame/BingoGame.vb
@@ -11,9 +11,9 @@ Option Strict On
 Module BingoGame
 
     'TODO
-    '[]Create an array to track the numbers called
-    '[]create a function to display the balls already called
-    '[]create a way to draw a ball, check if it has been called, and track it in...
+    '[~]Create an array to track the numbers called
+    '[~]create a function to display the balls already called
+    '[~]create a way to draw a ball, check if it has been called, and track it in...
     '[]create a way to restart the game.  Should happen automatically if all balls are called
     '[]new game, play again, or quit functionality
     '[]generate player cards and make playable
@@ -26,18 +26,31 @@ Module BingoGame
         'ball number = (letter index * 15) + (number index + 1)
 
         Dim bingoCage(4, 14) As Boolean
+        Dim exitFlag As Boolean = False
+        Dim userInput As String
 
-        'bingoCage(2, 5) = True
-        'bingoCage(4, 10) = True
-        'bingoCage(4, 7) = True
-        DisplayDraws(bingoCage)
+
+        Do Until exitFlag = True
+            userInput = Console.ReadLine()
+
+            If userInput = "Q" Or userInput = "q" Then
+                exitFlag = True
+            Else
+                DrawBall(bingoCage)
+                DisplayDraws(bingoCage)
+            End If
+
+        Loop
         Console.Read()
+
+
     End Sub
 
-    Sub DisplayDraws(ballCage(,) As Boolean)
+    Sub DisplayDraws(ByVal ballCage(,) As Boolean)
         Dim currentBall As String
         Dim columnWidth As Integer = 5
         Dim header = New String() {"B |", "I |", "N |", "G |", "O |"}
+        Console.Clear()
 
         'Display column headers
         For i = LBound(header) To UBound(header)
@@ -60,5 +73,21 @@ Module BingoGame
         Next
         Console.WriteLine(StrDup(25, "-"))
     End Sub
+
+    Sub DrawBall(ByRef bingoCage(,) As Boolean)
+        Dim letter As Integer
+        Dim number As Integer
+
+        Do
+            letter = RandomNumber(4)
+            number = RandomNumber(14)
+        Loop Until bingoCage(letter, number) = False
+        bingoCage(letter, number) = True
+    End Sub
+
+    Function RandomNumber(max As Integer) As Integer
+        Randomize(DateTime.Now.Millisecond * DateTime.Now.Second)
+        Return CInt(Rnd() * max)
+    End Function
 
 End Module
